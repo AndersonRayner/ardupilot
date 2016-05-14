0381933c85239d1a49463d6dcb5a74271b32e114
AndersonRayner <anderson_rayner@hotmail.com>
2016-05-14 19:11:46 +1000

    Removed the last references to AP_RPM from the wingtip stuff for APM:Copter

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -925,7 +925,7 @@ const AP_Param::Info Copter::var_info[] = {
 
     // @Group: WING
     // @Path: ../libraries/AP_Wingtip/AP_Wingtip.cpp
-    GOBJECT(wingtip_sensor, "WING", AP_RPM),
+    GOBJECT(wingtip_sensor, "WING", AP_Wingtip),
 
     // @Group: ADSB_
     // @Path: ../libraries/AP_ADSB/AP_ADSB.cpp
