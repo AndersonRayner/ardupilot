19954460afbe70f8704971bdac1642810da0c617
Matt <anderson_rayner@hotmail.com>
2016-08-01 14:39:37 +1000

    ArduCopter: Log airspeed when CTUN logging enabled

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -400,6 +400,7 @@ void Copter::ten_hz_logging_loop()
     }
     if (should_log(MASK_LOG_CTUN)) {
         attitude_control.control_monitor_log();
+        DataFlash.Log_Write_Airspeed(airspeed);
     }
 #if FRAME_CONFIG == HELI_FRAME
     Log_Write_Heli();
